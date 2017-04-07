359fbdd05e8e183fb0f961bbead113f92974f7b9
Oleksandr <xander_uzzzer@mail.ru>
2017-04-07 16:43:11 +0300

    0704171643

---
+++ Lesson6.Classes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6.Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = new string('*', 10) + "    Lesson 6    " + new string('*', 10);
+            Console.WriteLine("Lesson 6");
+
+            Console.ReadLine();
+        }
+    }
+}
